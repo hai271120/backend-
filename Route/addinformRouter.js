ed61e73cbaf3b9235ab2ee85f18e1a56579842e8
hai271120 <69028228+hai271120@users.noreply.github.com>
2021-06-17 19:50:18 +0700

    Revert "Add files via upload"
    This reverts commit d7852799fb5432733e7bdedebe2f4cbb1e4e9634.

--- Route/addinformRouter.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router()
-const addinformctrl = require('../Controller/addinformController')
-
-router.post("/addinform",addinformctrl.addinform)
-module.exports = router
--- Route/productRouter.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const productCtrl = require('../Controller/productController')
-router.route('/product')
-    .get(productCtrl.getproduct)
-    .post(productCtrl.createProduct)
-    module.exports = router
